ec9f044c3dce394af3ef3a82ba45bdc594058116
Kunaal-k <71382947+Kunaal-k@users.noreply.github.com>
2026-03-31 21:57:20 +0530

    Initial project structure and class definitions

---
+++ FitnessTracker.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+// Class to represent a single day of fitness data
+class DailyLog {
+    String date;
+    int treadmillMinutes;
+    String weightliftingFocus;
+    int proteinGrams;
+    
+    // Logic for this class will be added in the next update
+}
+
+public class FitnessTracker {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Personal Fitness Tracker!");
+        // Main logic to be implemented
+    }
+}

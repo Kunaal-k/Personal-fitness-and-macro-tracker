51155ab9f648ec0ef761a6211164dccebddf07fc
Kunaal-k <71382947+Kunaal-k@users.noreply.github.com>
2026-03-31 22:13:28 +0530

    Added DailyLog constructor and data display formatting

--- FitnessTracker.java
+++ FitnessTracker.java
@@ -1,19 +1,29 @@
 import java.util.ArrayList;
 import java.util.Scanner;
 
-// Class to represent a single day of fitness data
 class DailyLog {
     String date;
     int treadmillMinutes;
     String weightliftingFocus;
     int proteinGrams;
-    
-    // Logic for this class will be added in the next update
+
+    // Constructor to initialize the variables
+    public DailyLog(String date, int treadmillMinutes, String weightliftingFocus, int proteinGrams) {
+        this.date = date;
+        this.treadmillMinutes = treadmillMinutes;
+        this.weightliftingFocus = weightliftingFocus;
+        this.proteinGrams = proteinGrams;
+    }
+
+    // Method to display the log details
+    public void displayLog() {
+        System.out.println("Date: " + date + " | Cardio: " + treadmillMinutes + " mins | Lift: " + weightliftingFocus + " | Protein: " + proteinGrams + "g");
+    }
 }
 
 public class FitnessTracker {
     public static void main(String[] args) {
         System.out.println("Welcome to the Personal Fitness Tracker!");
-        // Main logic to be implemented
+        // Ready to implement user interaction
     }
 }
